c2bd1075ec5da7f373b6c7fde018ecb81061b89b
Antonio Faldetta <fuanton.io@proton.me>
2025-12-12 16:02:30 +0000

    removing ghostty. fixing flatpak

--- flatpak/fpsetup.sh
+++ flatpak/fpsetup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-flatpak remote-delete -y fedora
-flatpak remote-add -y --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-flatpak install -y flathub io.github.kolunmi.Bazaar
-flatpak install -y flathub app.zen_browser.zen
-
+flatpak remote-delete fedora
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
