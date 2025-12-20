e05e29e07aaf083a4b20f61214589ca97ebb485e
Antonio Faldetta <fanton.io@icloud.com>
2025-12-20 22:51:02 +0000

    swap discover with bazaar

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,3 +5,9 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 
 sudo dnf copr enable atim/starship
 sudo dnf install fastfetch fzf neovim starship stow zoxide zsh
+sudo dnf install firefox ptyxis
+
+sudo systemctl disable packagekit
+sudo systemctl stop packagekit
+sudo dnf remove PackageKit discover
+flatpak install flathub io.github.kolunmi.Bazaar
