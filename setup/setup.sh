495a4cbec3fdc7e5b221e49f810e553366577e61
Antonio Faldetta <fanton.io@icloud.com>
2025-12-21 15:04:53 +0000

    more sudo

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,9 +7,9 @@ sudo dnf copr enable atim/starship
 sudo dnf install fastfetch fzf neovim starship stow zoxide zsh
 #sudo dnf install firefox ptyxis
 
-systemctl stop packagekit.service
-systemctl mask packagekit.service
-systemctl stop packagekit-offline-update.service
-systemctl mask packgekit-offline-update.service
+sudo systemctl stop packagekit.service
+sudo systemctl mask packagekit.service
+sudo systemctl stop packagekit-offline-update.service
+sudo systemctl mask packgekit-offline-update.service
 sudo dnf remove PackageKit plasma-discover
 flatpak install flathub io.github.kolunmi.Bazaar
